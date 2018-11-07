e0de762fe4fdcb9b0c1ad36d56b1c3107b00cf3d
Angel Herrera Medina <angelherrerax7@gmail.com>
2018-11-07 13:20:20 -0500

    Se cambió el resize de max a min

--- ChatFirebase/Extensions/UIImageExtensions.swift
+++ ChatFirebase/Extensions/UIImageExtensions.swift
@@ -12,7 +12,7 @@ extension UIImage {
     func resizeImageWith(newSize: CGSize) -> UIImage {
         let horizontalRatio = newSize.width / size.width
         let verticalRatio = newSize.height / size.height
-        let ratio = max(horizontalRatio, verticalRatio)
+        let ratio = min(horizontalRatio, verticalRatio)
         let newSize = CGSize(width: size.width * ratio, height: size.height * ratio)
         UIGraphicsBeginImageContextWithOptions(newSize, false, 0)
         draw(in: CGRect(origin: CGPoint(x: 0, y: 0), size: newSize))
